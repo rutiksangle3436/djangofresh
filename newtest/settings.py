6c7b2facfbc837adbe595253f9bd53ecb1cea9db
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-30 10:38:21 +0530

    init

--- newtest/settings.py
+++ newtest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
