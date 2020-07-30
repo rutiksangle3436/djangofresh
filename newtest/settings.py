f27b574cf2ee7f2c533e95a17d2639b5372d638a
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-30 21:42:26 +0530

    init

--- newtest/settings.py
+++ newtest/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
