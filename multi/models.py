b0e6650c2b3e25142950c66e49d6bb35130d670f
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-30 22:36:04 +0530

    init

--- multi/models.py
+++ multi/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Destination(models.Model):
-    name:models.CharField(max_length=100)
-    email:models.TextField()
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
