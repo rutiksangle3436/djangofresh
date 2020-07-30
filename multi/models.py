98022138080af303e819f3b644fcc7670caa7b57
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-30 22:36:34 +0530

    init

--- multi/models.py
+++ multi/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Destination(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    phone = models.IntegerField
