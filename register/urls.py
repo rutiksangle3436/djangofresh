7c74ac0862a907210541c6850f71d12f7a958db7
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-31 22:38:04 +0530

    check existing email id

--- register/urls.py
+++ register/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register,name="home"),
-    path('done', views.reg,name="home"),
+    path('', views.reg,name="home"),
 ]
--- register/views.py
+++ register/views.py
@@ -3,24 +3,25 @@ from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
-def register(request):
-    return render(request,'Student.html')
+
 
 def reg(request):
 
+    if request.method == "GET":
+            return render(request,'Student.html')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
 
-    first_name = request.POST['first_name']
-    last_name = request.POST['last_name']
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
+        if User.objects.filter(email=email).exists():
+            return render(request,'Student.html',{'status':"EMAIL ALREADY EXISTS"})    
+        else:    
+            user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
 
-    if User.objects.filter(email=email).exists():
-        return HttpResponse("EMAIL ALREADY EXISTS!")    
-    else:    
-        user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
+        user.save()
+        print("user created")
 
-    user.save()
-    print("user created")
-
-    return HttpResponse("DONE")
+        return HttpResponse("DONE")
