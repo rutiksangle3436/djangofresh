674f1e24833cd1ac4d7f39214f568d92d416f8d7
rutiksangle3436 <rutiksangle3436@gmail.com>
2020-07-31 20:16:36 +0530

    init:

--- multi/views.py
+++ multi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import os
 import psycopg2
+from .models import Destination
 
 
 
@@ -10,8 +11,7 @@ def home(request):
     return render(request,'home.html')
 
 def mult(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
-    res = val1 * val2
+    dests = Destination.objects.all()
 
-    return render(request,'mul.html',{'result':res})
+
+    return render(request,'mul.html',{'dests':dests})
